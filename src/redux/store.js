496aa019c93d59afff349753424ffba8ffdd6b1b
ashishrair500 <100395176+ashishrair500@users.noreply.github.com>
2023-11-07 15:39:56 +0530

    import related checks

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
 import { applyMiddleware,   createStore } from "redux"
 import { composeWithDevTools } from "redux-devtools-extension"
-import thunk from "redux-thunk"
+import thunk from "redux-thunk";
+import authReducer from "./reducers/authReducer";
+
 import rootReducer from "./reducers/index"
 
 const store = createStore(
