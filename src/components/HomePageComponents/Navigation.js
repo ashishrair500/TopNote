4a28443f532d6c0a99c90f56be602c035b1ea35d
Ashish Rai <100395176+ashishrair500@users.noreply.github.com>
2023-11-10 15:54:11 +0530

    Update Navigation.js

--- src/components/HomePageComponents/Navigation.js
+++ src/components/HomePageComponents/Navigation.js
@@ -11,9 +11,10 @@ const {isAuthenticated,user} = useSelector(state => state.auth)
 const dispatch = useDispatch();
 
 
+    //<Link className='navbar-brand ms-5' to='/'>Notes Adda</Link>
+          //  <span className='text-warning'>{user.displayName}</span>
 
   return  <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
-    //<Link className='navbar-brand ms-5' to='/'>Notes Adda</Link>
     <ul className='navbar-nav ms-auto me-5'>
     {
       isAuthenticated ? (
@@ -21,7 +22,6 @@ const dispatch = useDispatch();
           <li className='nav-item mx-2'>
           <p className='my-0 mt-1'>
             <span className='text-light'>Welcome,{user.displayName}</span>
-          //  <span className='text-warning'>{user.displayName}</span>
           </p>
        </li>
           <li className='nav-item mx-2'>
