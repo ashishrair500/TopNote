44d8c116eef35f75f0082fc8562bc6d1ec39a1c5
Ashish Rai <100395176+ashishrair500@users.noreply.github.com>
2023-11-10 16:09:31 +0530

    Update Navigation.js

--- src/components/HomePageComponents/Navigation.js
+++ src/components/HomePageComponents/Navigation.js
@@ -19,29 +19,18 @@ const dispatch = useDispatch();
     {
       isAuthenticated ? (
           <>
-          <li className='nav-item mx-2'>
-          <p className='my-0 mt-1'>
-            <span className='text-light'>Welcome {user.displayName}</span></p>
-       </li><li className='nav-item mx-2'><Link className='btn btn-primary btn-sm' to='/dashboard'>
-            Dashboard
-        </Link></li><li className='nav-item'><button className='btn btn-success btn-sm' 
-        onClick={() => dispatch(signOutUser())} >
-           LogOut
+      <li className='nav-item mx-2'><p className='my-0 mt-1'><span className='text-light'>Welcome {user.displayName}</span></p>
+       </li><li className='nav-item mx-2'><Link className='btn btn-primary btn-sm' to='/dashboard'>Dashboard
+        </Link></li><li className='nav-item'><button className='btn btn-success btn-sm' onClick={() => dispatch(signOutUser())}>LogOut
         </button>
         </li>
           </>
       ) : (
           <>
-          <li className='nav-item mx-2'>
-        <Link className='btn btn-primary btn-sm' to='/login'>
-            Login
-        </Link> </li>
-        <li className='nav-item'>
-        <Link className='btn btn-success btn-sm' to='/register'>
-           Register
-        </Link>
-        </li>
-          </>
+          <li className='nav-item mx-2'><Link className='btn btn-primary btn-sm' to='/login'>Login
+        </Link></li>
+        <li className='nav-item'><Link className='btn btn-success btn-sm' to='/register'>Register</Link></li>
+        </>
       )
     }
     </ul>
