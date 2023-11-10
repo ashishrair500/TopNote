1462ce8b644e01d864aa8e950a09a92d0e15b8c2
Ashish Rai <100395176+ashishrair500@users.noreply.github.com>
2023-11-10 16:13:24 +0530

    Update Navigation.js

--- src/components/HomePageComponents/Navigation.js
+++ src/components/HomePageComponents/Navigation.js
@@ -11,16 +11,16 @@ const {isAuthenticated,user} = useSelector(state => state.auth)
 const dispatch = useDispatch();
 
 
-    //<Link className='navbar-brand ms-5' to='/'>Notes Adda</Link>
           //  <span className='text-warning'>{user.displayName}</span>
 
   return  <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+      <Link className='navbar-brand ms-5' to='/'>Notes Adda</Link>
     <ul className='navbar-nav ms-auto me-5'>
     {
       isAuthenticated ? (
           <>
       <li className='nav-item mx-2'><p className='my-0 mt-1'><span className='text-light'>Welcome {user.displayName}</span></p>
-       </li><li className='nav-item mx-2'><Link className='btn btn-primary btn-sm' to='/dashboard'>Dashboard</Link>
+       </li><li className='nav-item mx-2'><Link className='btn btn-primary btn-sm' to='/dashboard'>Dashboard </Link>
     </li><li className='nav-item'><button className='btn btn-success btn-sm' onClick={() => dispatch(signOutUser())}>LogOut
         </button>
         </li>
