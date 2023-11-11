2a6f86551d1fb0b331dc9e48f154b704b3991084
Ashish Rai <100395176+ashishrair500@users.noreply.github.com>
2023-11-11 21:13:11 +0530

    Update Navigation.jsx

--- src/components/HomePageComponents/Navigation.jsx
+++ src/components/HomePageComponents/Navigation.jsx
@@ -20,8 +20,8 @@ const NavigationComponent = () => {
             <>
               <li className='nav-item mx-2'>
                 <p className='my-0 mt-1 mx-2'>
-                  <span className='text-light'>Welcome, </span>
-                  <span className='text-warning'>{user.displayName}</span>
+                  <span className='text-light'>Welcome: </span>
+                  <span className='text-warning'>{user.displayName.toUpperCase}</span>
                 </p>
               </li>
               <li className='nav-item mx-2'>
